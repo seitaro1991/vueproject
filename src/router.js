471e25478573a25c93dd811f9c082822f800638e
seitaro1991 <seitarohisakawa@gmail.com>
2018-12-01 16:09:41 +0900

    vue開始

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Items from "./views/Items.vue";
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/items",
+      name: "items",
+      component: Items
     },
     {
       path: "/about",
