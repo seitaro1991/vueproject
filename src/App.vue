1e02864c91cc8e3b643763a68adb5ff7e816640e
seitaro1991 <seitarohisakawa@gmail.com>
2018-12-04 21:59:09 +0900

    ヘッダー作成中

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>|
       <router-link to="/items">Items</router-link>
-    </div>
+    </div>-->
+    <header>
+      <img class="title_img" src="/img/title.png" alt="title">
+
+      <div class="seach">
+        <form id="form1" action="自分のサイトURL">
+          <input id="s" name="s" type="text" placeholder="seach...">
+        </form>
+      </div>
+
+      <i class="fas fa-book-open story_img"></i>
+      <i class="fas fa-tshirt wear_img"></i>
+      <i class="fas fa-sign-in-alt login_img"></i>
+    </header>
     <router-view/>
   </div>
 </template>
@@ -28,5 +41,99 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "メイリオ", Meiryo, "ヒラギノ角ゴ Pro W3",
+    "HIragino Kaku Gothic Pro W3", "HIragino Kaku Gothic Pro", Osaka,
+    "ＭＳ Ｐゴシック", "MS P Gothic", sans-serif;
+}
+
+button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+  appearance: none;
+}
+
+input {
+  border: solid 1px #e7e7e7;
+  background: none;
+}
+
+header {
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  height: 50px;
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 24;
+  width: 100%;
+  border-bottom: solid 1px #efefef;
+  justify-content: center;
+  align-items: center;
+}
+
+.title_img {
+  grid-column: 1/4;
+  grid-row: 1/2;
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.story_img {
+  grid-column: 10/11;
+  grid-row: 1/2;
+  max-width: 70%;
+  max-height: 70%;
+  margin: 0 auto;
+}
+
+.wear_img {
+  grid-column: 11/12;
+  grid-row: 1/2;
+  max-width: 70%;
+  max-height: 70%;
+  margin: 0 auto;
+}
+
+.login_img {
+  grid-column: 12/13;
+  grid-row: 1/2;
+  max-width: 70%;
+  max-height: 70%;
+  margin: 0 auto;
+}
+
+.seach {
+  margin: 0 auto;
+  grid-column: 4/9;
+  grid-row: 1/2;
+}
+
+/*フォーム全体*/
+#form1 {
+  position: relative; /*フォームの相対位置*/
+  max-width: 100%; /*フォームのサイズ*/
+}
+
+/*検索ボックス*/
+#sbox {
+  width: 100%;
+  height: 100%; /*検索ボックスの高さ*/
+  padding: 0 10px; /*テキスト位置調整*/
+  border-radius: 0; /*検索ボックスの角を丸める*/
+  background: #efefef; /*検索ボックスの背景カラー*/
+  border-radius: 6px;
+  outline: 0; /*クリック時の青い枠線消す*/
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
